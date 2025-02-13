c0e42e0a01d666f9802345e26a8c2516e0a5b191
arshiya726 <arshiya0861.becse24@chitkara.edu.in>
2025-02-13 17:26:31 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,53 +1,43 @@
-#include<stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    if(a==1){
-        printf("January");
-    }
-     else if(a==2){
-        printf("Febraury");
-    }
-     else if(a==3){
-        printf("March");
-    }
-     else if(a==4){
-        printf("April");
-    }
-    else if(a==5){
-        printf("May");
-    }
-        else if(a==6){
-        printf("June");
-    }
-        else if(a==7){
-        printf("July");
-    }
-        else if(a==7){
-        printf("August");
-    }
-        else if(a==8){
-        printf("September");
-    }
-         else if(a==10){
-        printf("October");
-    }
-         else if(a==11){
-        printf("November");
-    }
-         else if(a==12){
-        printf("December");
-    }
-    else{
-        printf("Invalid month");
-    }
+#include <stdio.h>
 
+int main() {
+    int month;
 
+    // Input the month number
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
 
-
-
-
+    // Determine the number of days in the specified month
+    switch (month) {
+        case 1:  // January
+        case 3:  // March
+        case 5:  // May
+        case 7:  // July
+        case 8:  // August
+        case 10: // October
+        case 12: // December
+            printf("31 days\n");
+            break;
+        case 4:  // April
+        case 6:  // June
+        case 9:  // September
+        case 11: // November
+            printf("30 days\n");
+            break;
+        case 2:  // February
+            printf("28 days\n");
+            break;
+        default:
+            printf("Invalid month number. Please enter a number between 1 and 12.\n");
+            break;
+    }
 
     return 0;
-
 }
+
+
+
+
+
+
+    
